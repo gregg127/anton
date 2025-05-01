cb8de98bc1ac329a56a66a3d21e1fb892ed770b5
Grzegorz Gołębiowski <grzegorz.golebiowski127@gmail.com>
2025-05-01 10:33:44 +0200

    Fix docs deployment

--- docs/deploy-docs.sh
+++ docs/deploy-docs.sh
@@ -1,4 +1,4 @@
 rm -rf site
 mkdocs build
-docker compose build
-docker compose push
+docker build --platform linux/amd64,linux/arm64 -t harbor.golebiowski.dev/services/anton-docs:latest -f docker/Dockerfile .
+docker push harbor.golebiowski.dev/services/anton-docs
