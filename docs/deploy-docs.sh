5a1eb7a9623151c87c5a33e07d868ea80f8787e9
Grzegorz Gołębiowski <grzegorz.golebiowski127@gmail.com>
2025-05-02 11:43:40 +0200

    Update anton docs deployment script

--- docs/deploy-docs.sh
+++ docs/deploy-docs.sh
@@ -1,4 +1,29 @@
+#!/bin/bash
+VERSION=1.0
+IMAGE_NAME=harbor.golebiowski.dev/services/anton-docs
+
+log() {
+  echo "$(date +"%Y-%m-%d %H:%M:%S") - $1"
+}
+
+log "Cleaning up old site directory..."
 rm -rf site
+
+log "Building MkDocs site..."
 mkdocs build
-docker build --platform linux/amd64,linux/arm64 -t harbor.golebiowski.dev/services/anton-docs:latest -f docker/Dockerfile .
-docker push harbor.golebiowski.dev/services/anton-docs
+
+log "Building Docker image with version $VERSION..."
+docker build --platform linux/amd64,linux/arm64 --build-arg VERSION="$VERSION" -t "$IMAGE_NAME:$VERSION" -f Dockerfile .
+
+log "Pushing Docker image to registry..."
+docker push "$IMAGE_NAME:$VERSION"
+
+log "Updating version in kustomization..."
+sed -i '' "s|$IMAGE_NAME:.*|$IMAGE_NAME:$VERSION|" kustomization/deployment.yaml
+
+log "Commiting version to the repostiory..."
+git add deploy-docs.sh
+git add kustomization/deployment.yaml
+git commit -m "Release documentation version $VERSION"
+
+log "Deployment completed successfully."
